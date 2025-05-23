ba12e5877d61088500410ad3fe055e0f608751d1
Lian <gj39981225@163.com>
2025-05-23 19:01:20 +0800

    添加 - 一张新的icon（暂时命名为icon2.jpg） - SocialButtons.vue是按钮组 - TopProfile为原头像组
    修改
    - 将头像组逻辑拆分为SocialButtons.vue、TopProfile
    - 简化index.vue逻辑

---
+++ yukikoi-frontend/components/index/SocialButtons.vue
@@ -0,0 +1,152 @@
+<template>
+    <div class="button-group">
+        <a class="button" href="https://qm.qq.com/cgi-bin/qm/qr?k=542-vFFNEi71QkHcsy8JIhdixiqwavga&jump_from=webapi&authKey=igZGlTBrzwv+iNr8HkQei0OHZrQtHMw706mXCthQaSd0RsvAFkwNrYPFzgOh2nTU">
+            <div class="icon"><i class="ri-qq-fill"></i></div>
+            <span>QQGroup</span>
+        </a>
+        <a class="button" href="https://x.com/Yosa04942475621">
+            <div class="icon"><i class="ri-twitter-x-fill"></i></div>
+            <span>Twitter</span>
+        </a>
+        <a class="button" href="https://space.bilibili.com/433677987">
+            <div class="icon"><i class="ri-bilibili-fill"></i></div>
+            <span>BiliBili</span>
+        </a>
+        <a class="button" href="https://github.com/Yueosa">
+            <div class="icon"><i class="ri-github-fill"></i></div>
+            <span>GitHub</span>
+        </a>
+        <a class="button" href="mailto:yichengxin7@gmail.com">
+            <div class="icon"><i class="ri-mail-fill"></i></div>
+            <span>Gmail</span>
+        </a>
+        <a class="button" href="/home">
+            <div class="icon"><i class="ri-home-line"></i></div>
+            <span>Home</span>
+        </a>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+$qq-color: #12B7F5;
+$twitter-color: #000;
+$bilibili-color: #FB7299;
+$github-color: #333;
+$gmail-color: #D93025;
+$home-color: #5a9bc6;
+
+.button-group {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+
+    a.button {
+        text-decoration: none;
+        color: inherit;
+    }
+
+    .button {
+        display: inline-block;
+        height: 60px;
+        width: 60px;
+        margin: 0 12px;
+        overflow: hidden;
+        background: #fff;
+        border-radius: 50px;
+        cursor: pointer;
+        transition: box-shadow 1s ease, width 0.3s ease-out;
+
+        &:hover {
+            box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.1);
+            width: 200px;
+
+            .icon i {
+                color: #fff;
+            }
+
+            span {
+                opacity: 1;
+                transform: translateX(+10px);
+            }
+        }
+
+        .icon {
+            display: inline-block;
+            height: 60px;
+            width: 60px;
+            text-align: center;
+            border-radius: 50px;
+            box-sizing: border-box;
+            line-height: 60px;
+            transition: all 0.3s ease-out;
+
+            i {
+                font-size: 25px;
+                line-height: 60px;
+                transition: all 0.3s ease-out;
+            }
+        }
+
+        span {
+            font-size: 20px;
+            font-weight: 600;
+            line-height: 60px;
+            margin-left: 10px;
+            opacity: 0;
+            transform: translateX(-30px);
+            transition: all 0.5s ease;
+            display: inline-block;
+            white-space: nowrap;
+        }
+
+        &:nth-child(1):hover .icon {
+            background: $qq-color;
+        }
+
+        &:nth-child(2):hover .icon {
+            background: $twitter-color;
+        }
+
+        &:nth-child(3):hover .icon {
+            background: $bilibili-color;
+        }
+
+        &:nth-child(4):hover .icon {
+            background: $github-color;
+        }
+
+        &:nth-child(5):hover .icon {
+            background: $gmail-color;
+        }
+
+        &:nth-child(6):hover .icon {
+            background: $home-color;
+        }
+
+        &:nth-child(1):hover span {
+            color: $qq-color;
+        }
+
+        &:nth-child(2):hover span {
+            color: $twitter-color;
+        }
+
+        &:nth-child(3):hover span {
+            color: $bilibili-color;
+        }
+
+        &:nth-child(4):hover span {
+            color: $github-color;
+        }
+
+        &:nth-child(5):hover span {
+            color: $gmail-color;
+        }
+
+        &:nth-child(6):hover span {
+            color: $home-color;
+        }
+    }
+}
+</style>
---
+++ yukikoi-frontend/components/index/TopProfile.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="top">
+        <img class="avatar" src="/avatar/icon2.jpg" alt="avatar">
+        <div class="info">
+            <p>欢迎来到我的世界, 这里没有神, 只有我构筑的代码、记忆和光</p>
+            <p>——Sakura</p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.top {
+    display: flex;
+    padding: 20px;
+    align-items: center;
+    gap: 20px;
+
+    .avatar {
+        width: 200px;
+        height: 200px;
+        border-radius: 50%;
+        border: 3px solid #bfcce5;
+        object-fit: cover;
+        box-shadow:
+            0 0 12px rgba(175, 180, 200, 0.3),
+            0 4px 12px rgba(120, 130, 160, 0.2);
+        background: radial-gradient(circle, #f6f6f6 0%, #d1cde3 100%);
+        padding: 4px;
+    }
+
+    .info {
+        padding: 10px 30px;
+        color: #000;
+        font-weight: bold;
+
+        p {
+            text-align: right;
+            line-height: 200%;
+            font-size: 20px;
+        }
+    }
+}
+</style>
