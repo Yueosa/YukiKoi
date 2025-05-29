5b5659e4b7bdbc9ce69a7065cb481ebc4a808f24
Lian <gj39981225@163.com>
2025-05-29 11:53:21 +0800

    test(/everwhispers): 页面结构
    关于页面设计的一些想法
    - 目前打算分为左右两屏做显示，使用按钮和动画切换

--- yukikoi-frontend/pages/everwhispers.vue
+++ yukikoi-frontend/pages/everwhispers.vue
@@ -1,3 +1,32 @@
 <template>
-    <p>这里什么都没有哦~</p>
+    <div class="main">
+        <!-- 小组件部分 -->
+        <div class="login">
+            <p></p>
+        </div>
+
+        <!-- 页面主体 -->
+        <div class="tehbody">
+            <!-- 我自己的部分 -->
+            <div class="me">
+                <div class="myinput">
+                    <p>这里是我的输入框</p>
+                </div>
+                <div class="mymessages">
+                    <p>这里是我的消息</p>
+                </div>
+            </div>
+            <!-- 大家留下的部分 -->
+            <div class="onther">
+                <div class="oninput">
+                    <p>这里是其他访客输入框的部分</p>
+                </div>
+                <div class="onmessages">
+                    <p>这里是访客的留言</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </template>
+
