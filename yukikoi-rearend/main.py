84ef816dd34a419990154de54a310a79bba5ec5a
Lian <gj39981225@163.com>
2025-06-02 20:06:23 +0800

    feat(yukikoi-rearend/): 添加后端功能
    开始准备everwhispers的后端开发

---
+++ yukikoi-rearend/main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+import httpx
+from typing import Any, Dict
+
+app = FastAPI()
+
+# 用于开发阶段，允许跨域
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ['*'],
+    allow_methods = ['*'],
+    allow_headers = ['*'],
+)
+
+@app.get('/api/notes/md')
+async def get_notesmd(url: str = Query('https://raw.githubusercontent.com/Yueosa/SakuraNotes/refs/heads/main/README.md', description='raw地址')) -> Dict[str, Any]:
+    """
+    获取指定 URL 的 Markdown 内容。
+
+    参数:
+    - url (str): 需要请求的原始地址。
+
+    返回:
+    - Dict[str, Any]: 包含请求内容或错误信息的字典。
+    """
+    try:
+        async with httpx.AsyncClient() as client:
+            r = await client.get(url, timeout = 10)
+            r.raise_for_status()
+            return {"content": r.text}
+    except Exception as e:
+        return {"error": str(e)}
+
+@app.get('/')
+def main() -> str:
+    """
+    默认主页
+
+    返回：
+    - str: 网站名
+    """
+    return "YUkiKoi"
